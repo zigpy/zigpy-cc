f5d62944520196c207e811b287411bcbb025c956
Sándor Balázs <balazs.sandor@vcc.live>
2020-03-06 22:52:26 +0100

    fix version update when release created

--- version.py
+++ version.py
@@ -3,10 +3,10 @@ import os
 import re
 import sys
 
-parts = sys.argv[1].replace('v', '').split('.')
+parts = re.sub(r'^.*/v?', '', sys.argv[1]).split('.')
 dir = os.path.dirname(os.path.realpath(__file__))
 
-with fileinput.FileInput(dir + '/zigpy_cc/__init__.py', inplace=True, backup='.bak') as file:
+with fileinput.FileInput(dir + '/zigpy_cc/__init__.py', inplace=True) as file:
     for line in file:
         line = re.sub(r'(MAJOR_VERSION =).*', '\\1 ' + parts[0], line)
         line = re.sub(r'(MINOR_VERSION =).*', '\\1 ' + parts[1], line)
