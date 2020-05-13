23e5cc937fd95dbf276c09b43a7cc8b6750a59e0
Sándor Balázs <balazs.sandor@vcc.live>
2020-05-13 21:50:30 +0200

    bump version

--- version.py
+++ version.py
@@ -10,5 +10,5 @@ with fileinput.FileInput(dir + "/zigpy_cc/__init__.py", inplace=True) as file:
     for line in file:
         line = re.sub(r"(MAJOR_VERSION =).*", "\\1 " + parts[0], line)
         line = re.sub(r"(MINOR_VERSION =).*", "\\1 " + parts[1], line)
-        line = re.sub(r"(PATCH_VERSION =).*", "\\1 " + parts[2], line)
+        line = re.sub(r"(PATCH_VERSION =).*", "\\1 \"" + parts[2] + "\"", line)
         print(line, end="")
