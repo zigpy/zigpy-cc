94139739a804985b32152a771c758d5bb38f029a
Sándor Balázs <balazs.sandor@vcc.live>
2020-08-13 20:47:06 +0200

    bump version

--- zigpy_cc/__init__.py
+++ zigpy_cc/__init__.py
@@ -1,6 +1,6 @@
 # This file updated automatically before PyPI release was created
 MAJOR_VERSION = 0
-MINOR_VERSION = 4
-PATCH_VERSION = "4"
+MINOR_VERSION = 5
+PATCH_VERSION = "0"
 __short_version__ = "{}.{}".format(MAJOR_VERSION, MINOR_VERSION)
 __version__ = "{}.{}".format(__short_version__, PATCH_VERSION)
